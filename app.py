a4d92211de05b391408cc96eeffbd7990d419167
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-10-06 13:32:26 +0300

    route

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='f6f18f2b875e6effe3f747230b0d1ea1742e781f23ec1603'
 
 
+
 courses_list = [{
-    "title": "Python 101",
-    "description": "Learn python basic",
-    "price": 35,
-    "available": 'True',
-    "level": "Beginner"
-}]
+    'title': 'Python 101',
+    'description': 'Learn Python basics',
+    'price': 34,
+    'available': True,
+    'level': 'Beginner'
+    }]
+
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
     form = CourseForm()
-    return render_template("index.html", form=form)
+    return render_template('index.html', form=form)
